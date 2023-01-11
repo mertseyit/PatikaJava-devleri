ca86e0884951a3afdc5c190a12b749da3e1a96b3
mert seyit <99680812+mertseyit@users.noreply.github.com>
2023-01-11 17:59:58 +0300

    Yazim hataları düzeltildi

--- OgrenciNotUygulamasi/src/Main.java
+++ OgrenciNotUygulamasi/src/Main.java
@@ -4,17 +4,17 @@ public class Main {
     public static void main(String[] args) {
         double mat,muzik,kimya,turkce,tarih,fizik;
         Scanner input = new Scanner(System.in);
-        System.out.println("Ogrencini mat notunu giriniz");
+        System.out.println("Ogrencinin mat notunu giriniz");
         mat = input.nextDouble();
-        System.out.println("Ogrencini muzik notunu giriniz");
+        System.out.println("Ogrencinin muzik notunu giriniz");
         muzik = input.nextDouble();
-        System.out.println("Ogrencini kimya notunu giriniz");
+        System.out.println("Ogrencinin kimya notunu giriniz");
         kimya = input.nextDouble();
-        System.out.println("Ogrencini türkçe notunu giriniz");
+        System.out.println("Ogrencinin türkçe notunu giriniz");
         turkce = input.nextDouble();
-        System.out.println("Ogrencini tarih notunu giriniz");
+        System.out.println("Ogrencinin tarih notunu giriniz");
         tarih = input.nextDouble();
-        System.out.println("Ogrencini fizik notunu giriniz");
+        System.out.println("Ogrencinin fizik notunu giriniz");
         fizik = input.nextDouble();
 
         double ort = (mat+muzik+kimya+turkce+tarih+fizik)/6;
