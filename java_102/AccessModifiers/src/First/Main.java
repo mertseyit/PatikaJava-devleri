4ba710db573518546e4d9c5a82f4a9d2aae72717
= <=>
2023-02-18 11:14:50 +0300

    update

--- java_102/AccessModifiers/src/First/Main.java
+++ java_102/AccessModifiers/src/First/Main.java
@@ -2,7 +2,7 @@ package First; // hangi pakette bulunduğunu gösterir
 
 public class Main {
     public static void main(String[] args) {
-        SayHello message = new SayHello("mert");
+        SayHello message = new SayHello("mert seyit yılmaz");
         System.out.println(SayHello.counter); // counter değişkeni bir sınıf değişkenidir ve direkt olarak sınıf üzerinden kullanılabilir
         message.printMessage();
         System.out.println(message.pacgakeName); // pacgakeName sadece ait olduğu paket içerisinde bir değişken olarak kullanılabilir
