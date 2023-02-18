2fac71e7b2b58dc367a76c45d29f8171753b9ecf
= <=>
2023-02-18 11:19:36 +0300

    added final variables

--- java_102/AccessModifiers/src/First/Main.java
+++ java_102/AccessModifiers/src/First/Main.java
@@ -2,6 +2,7 @@ package First; // hangi pakette bulunduğunu gösterir
 
 public class Main {
     public static void main(String[] args) {
+        final double PI = 3.14; // final değiştirilemez bir değişken oluşturmak için kullanılır (exp: const)
         SayHello message = new SayHello("mert seyit yılmaz and his friends");
         System.out.println(SayHello.counter); // counter değişkeni bir sınıf değişkenidir ve direkt olarak sınıf üzerinden kullanılabilir
         message.printMessage();
